8b852e41453f0d9474441a113b356ff2dacae696
lemanrza <lemaner@code.edu.az>
2025-02-18 01:52:54 +0400

    artists section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper1", {
     loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 26,
+    autoplay: {
+        delay: 3000, 
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
